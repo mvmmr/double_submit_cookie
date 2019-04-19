76cadd77365a8d603a3a379c5510c0825cf9d9d7
muammar <muammararif007@gmail.com>
2019-04-19 16:30:45 +0530

    add checking CSRF token to form submission page

--- form_submit.php
+++ form_submit.php
@@ -7,7 +7,20 @@
     <?php include_once "header.php" ?>
 </head>
 <body>
-    <?php // TODO: Implement form submission ?>
+    <?php
+    if (!empty($_POST) && isset($_POST["csrf"]) && isset($_SESSION["logged_in"]) && isset($_COOKIE["csrf_token"])) {
+        $csrf_token = $_COOKIE["csrf_token"];
+        if ($csrf_token == $_POST["csrf"]) {
+            echo "<p><b>MESSAGE:</b> ".htmlspecialchars($_POST["msg"], ENT_COMPAT, 'UTF-8')."</p>";
+        } else {
+            echo "<h2 style='color:red'>TOKEN INVALID</h2>";
+        }
+        echo "<p><b>TOKEN:</b> ".$_POST["csrf"]."</p>";
+        echo "<p><b>COOKIE:</b> ".$csrf_token."</p>";
+    } else {
+        echo "<h2 style='color:red'>ERROR</h2>";
+    }
+    ?>
     <hr>
     <a href="form.php">Back</a>
 </body>
