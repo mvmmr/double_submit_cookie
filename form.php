9bda6bc69645b4713b32a67c4819cb7b15968a37
muammar <muammararif007@gmail.com>
2019-04-19 15:55:31 +0530

    load csrf token from the cookie in form.php

--- form.php
+++ form.php
@@ -7,7 +7,34 @@
     <script>
         'use strict';
         function getToken() {
-            // TODO: Get token from cookie
+            var cookies = document.cookie.split(';');
+            var csrfSearch = "csrf_token=";
+            var csrfToken;
+            var el;
+
+            // find csrf cookie
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = cookies[i].trim();
+                if (cookie.indexOf(csrfSearch) > -1) {
+                    csrfToken = cookie.substring(csrfSearch.length);
+                    break;
+                }
+            }
+
+            // add csrf token to form
+            if (csrfToken != null) {
+                el = document.createElement("input");
+                el.name = "csrf";
+                el.type = "text";
+                el.hidden = true;
+                el.value = csrfToken;
+            } else {
+                el = document.createElement("p");
+                el.className = "error-msg";
+                el.innerText = "Error: cookie not found!";
+            }
+
+            document.getElementById("testForm").appendChild(el);
         }
 
         document.addEventListener("DOMContentLoaded", function() {
